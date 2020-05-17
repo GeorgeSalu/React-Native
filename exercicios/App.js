c3443d5165a10d7bb268704eb3a25d8dff0d2dd4
George Salu <george.salu10@gmail.com>
2020-05-16 22:44:56 -0300

    recriando o componente App

--- exercicios/App.js
+++ exercicios/App.js
@@ -4,9 +4,20 @@ import {View, Text, StyleSheet} from 'react-native'
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>App!</Text>
+      <View style={styles.container}>
+        <Text style={styles.f20}>App!</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'  
+  },
+  f20: {
+    fontSize: 20
+  }
+})
