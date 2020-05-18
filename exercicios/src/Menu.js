f7fb73bbc602f18afaf20c8b3d0963ea8dda0c49
George Salu <george.salu10@gmail.com>
2020-05-18 10:04:51 -0300

    diferencas entre android e ios

--- exercicios/src/Menu.js
+++ exercicios/src/Menu.js
@@ -6,12 +6,15 @@ import Simples from './componentes/Simples'
 import ParImpar from './componentes/ParImpar'
 import { Inverter, MegaSena } from './componentes/Multi'
 import Contador from './componentes/Contador'
+import Plataformas from './componentes/Plataformas'
+import { Platform } from 'react-native'
 
 const megasena = () => <MegaSena numeros={8} />
 const parimpar = () => <ParImpar numero={20} />
 const inverter = () => <Inverter texto="texto invertido"/>
 const simples = () => <Simples texto="Teste" />
 const contador = () => <Contador numeroInicial={100} />
+const plataforma = () => <Plataformas />
 
 const Drawer = createDrawerNavigator();
 
@@ -19,6 +22,7 @@ export default function Menu() {
   return (
     <NavigationContainer>
       <Drawer.Navigator initialRouteName="Simples">
+        <Drawer.Screen name="Plataformas" component={plataforma } />
         <Drawer.Screen name="Contador" component={contador } />
         <Drawer.Screen name="Mega Sena" component={megasena } />
         <Drawer.Screen name="Par & Ímpar" component={parimpar } />
