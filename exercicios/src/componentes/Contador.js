6c6f37ed32eb6885878d12097b3e1b2ed50e70e5
George Salu <george.salu10@gmail.com>
2020-05-18 09:48:05 -0300

    cuidados com o this

--- exercicios/src/componentes/Contador.js
+++ exercicios/src/componentes/Contador.js
@@ -7,7 +7,7 @@ export default class Contador extends Component {
     numero: this.props.numeroInicial
   }
 
-  maisUm = () => {
+  maisUm() {
     this.setState({ numero: this.state.numero + 1 })
   }
 
@@ -20,7 +20,7 @@ export default class Contador extends Component {
       <View>
         <Text style={{fontSize: 40}}>{this.state.numero}</Text>
         <TouchableHighlight 
-          onPress={this.maisUm}
+          onPress={() => this.maisUm()}
           onLongPress={this.limpar}>
           <Text>Incrementar/Zerar</Text>
         </TouchableHighlight>
