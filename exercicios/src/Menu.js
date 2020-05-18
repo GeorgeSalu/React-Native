492c47c1a5ef4c74a64c08a1769308bc277a5f2d
George Salu <george.salu10@gmail.com>
2020-05-17 21:52:03 -0300

    adicionando navegação ao projeto

---
+++ exercicios/src/Menu.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { NavigationContainer } from '@react-navigation/native'
+
+import Simples from './componentes/Simples'
+import ParImpar from './componentes/ParImpar'
+import { Inverter, MegaSena } from './componentes/Multi'
+
+const megasena = () => <MegaSena numeros={8} />
+const parimpar = () => <ParImpar numero={20} />
+const inverter = () => <Inverter texto="texto invertido"/>
+const simples = () => <Simples texto="Teste" />
+
+const Drawer = createDrawerNavigator();
+
+export default function Menu() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Simples">
+        <Drawer.Screen name="Megasena" component={megasena } />
+        <Drawer.Screen name="Parimpar" component={parimpar } />
+        <Drawer.Screen name="Inverter" component={inverter } />
+        <Drawer.Screen name="Simples" component={simples } />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
