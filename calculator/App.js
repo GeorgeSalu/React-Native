9ab86404ccb6a3db8b6a93734e7d16277d6e92bb
George Salu <george.salu10@gmail.com>
2020-05-19 09:39:07 -0300

    mudando para um componente de classe

--- calculator/App.js
+++ calculator/App.js
@@ -1,47 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
-  Text,
-  StatusBar,
 } from 'react-native';
 import Button from './src/components/Button'
-
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 import Display from './src/components/Display';
 
-const App: () => React$Node = () => {
-  return (
-    <View style={styles.container}>
-      <View style={styles.buttons}>
-        <Button label='AC' />
-        <Button label='/' />
-        <Button label='7' />
-        <Button label='8' />
-        <Button label='9' />
-        <Button label='*' />
-        <Button label='4' />
-        <Button label='5' />
-        <Button label='6' />
-        <Button label='-' />
-        <Button label='1' />
-        <Button label='2' />
-        <Button label='3' />
-        <Button label='+' />
-        <Button label='0' />
-        <Button label='.' />
-        <Button label='=' />
+class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.buttons}>
+          <Button label='AC' />
+          <Button label='/' />
+          <Button label='7' />
+          <Button label='8' />
+          <Button label='9' />
+          <Button label='*' />
+          <Button label='4' />
+          <Button label='5' />
+          <Button label='6' />
+          <Button label='-' />
+          <Button label='1' />
+          <Button label='2' />
+          <Button label='3' />
+          <Button label='+' />
+          <Button label='0' />
+          <Button label='.' />
+          <Button label='=' />
+        </View>
       </View>
-    </View>
-  );
+    )
+  }
 };
 
 const styles = StyleSheet.create({
