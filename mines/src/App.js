a2999e9c1265a26a177db8e327425897d1c223e6
George Salu <george.salu10@gmail.com>
2020-05-21 13:00:54 -0300

    componente tabuleiro (MineField)

--- mines/src/App.js
+++ mines/src/App.js
@@ -2,31 +2,57 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  StyleSheet
 } from 'react-native';
 import params from './params'
 import Field from './components/Field'
 import Flag from './components/Flag'
+import MineField from './components/MineField'
+import { createMineBoard } from './functions'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+
+    return {
+      board: createMineBoard(rows, cols, this.minesAmount())
+    }
+  }
+
   render() {
     return (
       <View >
         <Text >Iniciando o mines</Text>
         <Text>Tamanho da grade: 
           {params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged />
-        <Field flagged opened/>
-        <Flag bigger />
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end'
+  },
+  board: {
+    alignItems: 'center',
+    backgroundColor: '#AAA'
+  }
+})
