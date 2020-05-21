763225755003f21a03693fa1d329af5d57be66d2
George Salu <george.salu10@gmail.com>
2020-05-21 11:12:27 -0300

    componente mina

--- mines/src/components/Mine.js
+++ mines/src/components/Mine.js
@@ -12,3 +12,25 @@ export default props => {
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  coreMine: {
+    height: 14,
+    width: 14,
+    borderRadius: 10,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  line: {
+    position: 'absolute',
+    height: 3,
+    width: 20,
+    borderRadius: 3,
+    backgroundColor: 'black'
+  }
+})
