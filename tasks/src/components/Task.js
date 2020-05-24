0ec0aa75b896968a174c78d8ab5250c7b67a44e2
George Salu <george.salu10@gmail.com>
2020-05-24 13:10:24 -0300

    componente Tarefa #03

--- tasks/src/components/Task.js
+++ tasks/src/components/Task.js
@@ -1,14 +1,36 @@
 import React from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome'
 
 export default props => {
   return (
     <View style={styles.container}>
-      <Text>{props.desc}</Text>
-      <Text>{props.estimateAt + ""}</Text>
-      <Text>{props.doneAt + ""}</Text>
+      <View style={styles.checkContainer}>
+        {getCheckView(props.doneAt)}
+      </View>
+      <View>
+        <Text>{props.desc}</Text>
+        <Text>{props.estimateAt + ""}</Text>
+      </View>
     </View>
   )
+}
+
+function getCheckView(doneAt) {
+  if(doneAt != null) {
+    return (
+      <View>
+        <Text>Concluida</Text>
+      </View>
+    )
+  } else {
+    return (
+      <View>
+        <Text>Pendente</Text>
+      </View>
+    )
+  }
+
 }
 
 const styles = StyleSheet.create({
@@ -18,5 +40,8 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     alignItems: 'center',
     paddingVertical: 10
+  },
+  checkContainer: {
+    width: '20%'
   }
 })
