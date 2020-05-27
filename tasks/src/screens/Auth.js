9e79d7e04b6bc67674bff5cd2dbfbc0338768875
George Salu <george.salu10@gmail.com>
2020-05-27 10:12:42 -0300

    tela de autenticacao #03

--- tasks/src/screens/Auth.js
+++ tasks/src/screens/Auth.js
@@ -15,8 +15,11 @@ import commonStyles from '../commonStyles'
 export default class Auth extends Component {
 
   state = {
+    name: '',
     email: '',
-    password: ''
+    password: '',
+    confirmPassword: '',
+    stageNew: true
   }
 
   render() {
@@ -24,13 +27,30 @@ export default class Auth extends Component {
       <ImageBackground source={backgroundImage} style={styles.background}>
         <Text style={styles.title}>Tasks</Text>
         <View style={styles.formContainer}>
+          <Text style={styles.subtitle}>
+            {this.state.stageNew ? 'Crie a sua conta' : 'Informe seus dados'}
+          </Text>
+          {this.state.stageNew &&
+          <TextInput placeholder='Nome' value={this.state.name}
+            style={styles.input} onChangeText={name => this.setState({ name })} />
+          }
           <TextInput placeholder='E-mail' value={this.state.email}
             style={styles.input} onChangeText={email => this.setState({ email })} />
           <TextInput placeholder='Senha' value={this.state.password}
-            style={styles.input} onChangeText={password => this.setState({ password })} />
+            style={styles.input} 
+            onChangeText={password => this.setState({ password })}
+            secureTextEntry={true} />
+          {this.state.stageNew &&
+          <TextInput placeholder='Confirmação de Senha' value={this.state.confirmPassword}
+            style={styles.input} 
+            onChangeText={confirmPassword => this.setState({ confirmPassword })}
+            secureTextEntry={true} />
+          }
           <TouchableOpacity>
             <View style={styles.button}>
-              <Text style={styles.buttonText}>Entrar</Text>
+              <Text style={styles.buttonText}>
+                {this.state.stageNew ? 'Registrar' : 'Entrar'}
+              </Text>
             </View>
           </TouchableOpacity>
         </View>
@@ -52,6 +72,13 @@ const styles = StyleSheet.create({
     fontSize: 70,
     marginBottom: 10
   },
+  subtitle: {
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 20,
+    color: '#FFF',
+    textAlign: 'center',
+    marginBottom: 10
+  },  
   input: {
     marginTop: 10,
     backgroundColor: '#FFF',
