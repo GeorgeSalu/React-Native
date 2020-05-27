785325ae2ad5259f179b1bb40d49f96c83b950ff
George Salu <george.salu10@gmail.com>
2020-05-27 10:35:41 -0300

    navegação

---
+++ tasks/src/Navigator.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+
+import Auth from './screens/Auth'
+import TaskList from './screens/TaskList'
+
+const MainRoutes = {
+  Auth: {
+    name: 'Auth',
+    screen: Auth
+  },
+  Home: {
+    name: "Home",
+    screen: TaskList
+  }
+}
+
+const MainNavigator = createSwitchNavigator(MainRoutes, {
+  initialRouteName: 'Auth'
+})
+
+export default createAppContainer(MainNavigator)
