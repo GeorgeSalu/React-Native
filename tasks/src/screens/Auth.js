e00a5d7ac2a75219f5b6ab8fd4db7fc01220c8f8
George Salu <george.salu10@gmail.com>
2020-05-27 11:33:40 -0300

    integrando autenticação com backend #01

--- tasks/src/screens/Auth.js
+++ tasks/src/screens/Auth.js
@@ -8,7 +8,7 @@ import {
   Platform,
   Alert
 } from 'react-native'
-
+import axios from 'axios'
 import backgroundImage from '../../assets/imgs/login.jpg'
 import commonStyles from '../commonStyles'
 import AuthInput from '../components/AuthInput'
@@ -27,9 +27,25 @@ export default class Auth extends Component {
 
   signinOrSignup = () => {
     if(this.state.stageNew) {
-      Alert.alert('Sucesso', 'criar conta')
+      this.signup()
     } else {
       Alert.alert('Sucesso', 'logar')
+    }
+  }
+
+  signup = async () => {
+    try {
+      await axios.post(`${server}/signup`, {
+        name: this.state.name,
+        email: this.state.email,
+        password: this.state.password,
+        confirmPassword: this.state.confirmPassword,
+      })
+
+      showSuccess('Usuario cadastrado')
+      this.setState({ stageNew: false })
+    } catch (error) {
+      showError(error)
     }
   }
 
