aa3c1b5fd7318cecccfd0ab2236d000afd1aacbc
George Salu <george.salu10@gmail.com>
2020-05-27 16:57:48 -0300

    informações de usuario #02

--- tasks/src/screens/Menu.js
+++ tasks/src/screens/Menu.js
@@ -2,20 +2,22 @@ import React from 'react'
 import {ScrollView, View, Text, StyleSheet} from 'react-native'
 import {DrawerItems} from 'react-navigation-drawer'
 import {Gravatar} from 'react-native-gravatar'
+import commonStyles from '../commonStyles'
 
 export default props => {
 
   return (
     <ScrollView>
       <View style={styles.header}>
+        <Text style={styles.title}>Tasks</Text>
         <Gravatar style={styles.avatar} 
           options={{
             email: props.navigation.getParam('email'),
             secure: true
           }} />
           <View style={styles.userInfo}>
-            <Text>{props.navigation.getParam('name')}</Text>
-            <Text>{props.navigation.getParam('email')}</Text>
+            <Text style={styles.name}>{props.navigation.getParam('name')}</Text>
+            <Text style={styles.email}>{props.navigation.getParam('email')}</Text>
           </View>
       </View>
       <DrawerItems {...props} />
@@ -28,11 +30,30 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderColor: '#DDD',
   },
+  title: {
+    color: '#000',
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 30,
+    paddingTop: 30,
+    padding: 10
+  },
   avatar: {
     width: 60,
     height: 60,
     borderWidth: 3,
     borderRadius: 30,
-    margin: 10
+    margin: 10,
+    backgroundColor: '#222'
+  },
+  userInfo: {
+    marginLeft: 10
+  },
+  name: {
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 20
+  },
+  email: {
+    fontFamily: commonStyles.fontFamily,
+    fontSize: 15
   }
 })
