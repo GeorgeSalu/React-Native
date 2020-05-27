ecab69bfa5218a0bb6fa328f736f8f02bbf4e40e
George Salu <george.salu10@gmail.com>
2020-05-27 15:35:33 -0300

    drawer navigator #02

--- tasks/src/screens/TaskList.js
+++ tasks/src/screens/TaskList.js
@@ -47,7 +47,9 @@ export default class TaskList extends Component {
 
   loadTasks = async () => {
     try {
-      const maxDate = moment().format('YYYY-MM-DD 23:59:59')
+      const maxDate = moment()
+        .add({ days: this.props.daysAhead })
+        .format('YYYY-MM-DD 23:59:59')
       const res = await axios.get(`${server}/tasks?date=${maxDate}`)
       this.setState({ tasks: res.data }, this.filterTasks)
 
@@ -123,6 +125,11 @@ export default class TaskList extends Component {
         <ImageBackground source={todayImage} 
           style={styles.background}>
             <View style={styles.iconBar}>
+              <TouchableOpacity onPress={() => this.props.navigation.openDrawer()}>
+                <Icon name={'bars'}
+                  size={20} color={commonStyles.colors.secondary}></Icon>
+              </TouchableOpacity>
+              
               <TouchableOpacity onPress={this.toggleFilter}>
                 <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
                   size={20} color={commonStyles.colors.secondary}></Icon>
@@ -181,7 +188,7 @@ const styles = StyleSheet.create({
   iconBar: {
     flexDirection: 'row',
     marginHorizontal: 20,
-    justifyContent: 'flex-end',
+    justifyContent: 'space-between',
     marginTop: Platform.OS === 'ios' ? 40 : 10
   },
   addButton: {
