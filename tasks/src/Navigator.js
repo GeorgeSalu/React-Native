7c4fadaa47b7427e5940b71de2f50d72b597c848
George Salu <george.salu10@gmail.com>
2020-05-27 15:25:11 -0300

    drawer navigator #01

--- tasks/src/Navigator.js
+++ tasks/src/Navigator.js
@@ -1,7 +1,42 @@
+import React from 'react'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation'
+import { createDrawerNavigator } from 'react-navigation-drawer'
 
 import Auth from './screens/Auth'
 import TaskList from './screens/TaskList'
+
+const menuRoutes = {
+  Today: {
+    name: 'Today',
+    screen: props => <TaskList title='Hoje' daysAhead={0} {...props} />,
+    navigationOptions: {
+      title: 'Hoje'
+    }
+  },
+  Tomorrow: {
+    name: 'Tomorrow',
+    screen: props => <TaskList title='Amanha' daysAhead={1} {...props} />,
+    navigationOptions: {
+      title: 'Amanha'
+    }
+  },
+  Week: {
+    name: 'Week',
+    screen: props => <TaskList title='Semana' daysAhead={7} {...props} />,
+    navigationOptions: {
+      title: 'Semana'
+    }
+  },
+  Month: {
+    name: 'Month',
+    screen: props => <TaskList title='Mes' daysAhead={30} {...props} />,
+    navigationOptions: {
+      title: 'Mes'
+    }
+  }
+}
+
+const menuNavigator = createDrawerNavigator(menuRoutes, )
 
 const mainRoutes = {
   Auth: {
@@ -10,7 +45,7 @@ const mainRoutes = {
   },
   Home: {
     name: "Home",
-    screen: TaskList
+    screen: menuNavigator
   }
 }
 
