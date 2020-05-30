48a55e86fe77567c5be092a66fb338570a112d0b
George Salu <george.salu10@gmail.com>
2020-05-30 16:50:37 -0300

    exluindo App.js

--- lambe/index.js
+++ lambe/index.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
 import Feed from './src/screens/Feed'
 import Navigator from './src/Navigator'
 import {name as appName} from './app.json';
