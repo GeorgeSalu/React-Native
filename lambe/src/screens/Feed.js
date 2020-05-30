d27893c4ae85c7722d2c50584ee061c00be7b5fc
George Salu <george.salu10@gmail.com>
2020-05-30 16:36:47 -0300

    tela de Feeds

--- lambe/src/screens/Feed.js
+++ lambe/src/screens/Feed.js
@@ -43,3 +43,14 @@ class Feed extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF'
+  }
+})
+
+export default Feed
