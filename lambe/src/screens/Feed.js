bf75fd5d31db4d0c63a4eb9ef7374ff6df25dae2
George Salu <george.salu10@gmail.com>
2020-05-30 16:33:07 -0300

    tela de Feeds

---
+++ lambe/src/screens/Feed.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import {
+  StyleSheet,
+  FlatList,
+  View
+} from 'react-native'
+import Header from '../components/Header'
+import Post from '../components/Post'
+
+class Feed extends Component {
+  state = {
+    posts: [{
+      id: Math.random(),
+      nickname: 'rafael pereira filho',
+      email: 'rafael@gmail.com',
+      image: require('../../assets/imgs/fence.jpg'),
+      comments: [{
+        nickname: 'john',
+        comment: 'stuning'
+      }, {
+        nickname: 'ana',
+        comment: 'foto linda'
+      }]
+    },{
+      id: Math.random(),
+      nickname: 'francisco pereira filho',
+      email: 'rafael@gmail.com',
+      image: require('../../assets/imgs/fence.jpg'),
+      comments: []
+    }]
+  }
+
+  render() {
+    return(
+      <View style={styles.container}>
+        <Header />
+        <FlatList 
+          data={this.state.posts}
+          keyExtractor={item => `${item.id}`}
+          renderItem={({ item }) => 
+            <Post key={item.id} {...item} /> }/>
+      </View>
+    )
+  }
+}
