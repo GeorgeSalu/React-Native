111a02e01a408697b943d003f5a00c06b0365303
George Salu <george.salu10@gmail.com>
2020-05-30 15:47:43 -0300

    componente comentarios

--- lambe/App.js
+++ lambe/App.js
@@ -5,10 +5,19 @@ import Post from './src/components/Post'
 
 export default class App extends Component {
   render() {
+    const comments = [{
+      nickname: 'joaa elena silva',
+      comment: 'excelente foto'
+    },{
+      nickname: 'rafael',
+      comment: 'muito ruim'
+    }]
+
     return (
       <View style={{ flex: 1 }}>
         <Header />
-        <Post image={require('./assets/imgs/fence.jpg')} />
+        <Post image={require('./assets/imgs/fence.jpg')}
+          comments={comments} />
       </View>
     );
   }
