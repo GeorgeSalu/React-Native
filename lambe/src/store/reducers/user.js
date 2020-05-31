400c634d02d5dfe15e9a5fa8f20a388b4d22470d
George Salu <george.salu10@gmail.com>
2020-05-31 14:26:11 -0300

    login com redux #01

--- lambe/src/store/reducers/user.js
+++ lambe/src/store/reducers/user.js
@@ -7,3 +7,24 @@ const initialState = {
   name: null,
   email: null
 }
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case USER_LOGGED_IN:
+      return {
+        ...state,
+        name: action.payload.name,
+        email: action.payload.email
+      }
+    case USER_LOGGED_OUT:
+      return {
+        ...state,
+        name: null,
+        email: null
+      } 
+    default:
+        return state
+  }
+}
+
+export default reducer
