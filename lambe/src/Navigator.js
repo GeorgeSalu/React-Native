d99f71fb15d4103c0e3432f7f4ab4d490a50160a
George Salu <george.salu10@gmail.com>
2020-05-30 21:54:02 -0300

    tela adicionar foto

--- lambe/src/Navigator.js
+++ lambe/src/Navigator.js
@@ -3,6 +3,7 @@ import { createBottomTabNavigator } from 'react-navigation'
 import Icon from 'react-native-vector-icons/FontAwesome'
 
 import Feed from './screens/Feed'
+import AddPhoto from './screens/AddPhoto'
 
 const MenusRoutes = {
   Feed: {
@@ -16,7 +17,7 @@ const MenusRoutes = {
   },
   Add: {
     name: 'AddPhoto',
-    screen: Feed,
+    screen: AddPhoto,
     navigationOptions: {
       title: 'Add Picture',
       tabBarIcon: ({ tintColor }) => 
--- lambe/src/screens/AddPhoto.js
+++ lambe/src/screens/AddPhoto.js
@@ -46,9 +46,54 @@ class AddPhoto extends Component {
           <TouchableOpacity onPress={this.pickImage} style={styles.buttom} >
             <Text style={styles.buttomText}>Escolha a foto</Text>
           </TouchableOpacity>
+          <TextInput placeholder='Algum comentario para a foto'
+            style={styles.input} value={this.state.comment}
+            onChangeText={comment => this.setState({ comment })} />
+
+          <TouchableOpacity onPress={this.save} style={styles.buttom}>
+            <Text style={styles.buttom}>Salvar</Text>
+          </TouchableOpacity>
         </View>
       </ScrollView>
     )
   }
 
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  title: {
+    fontSize: 20,
+    marginTop: Platform.OS === 'ios' ? 30 : 10,
+    fontWeight: 'bold'
+  },
+  imageContainer: {
+    width: '90%',
+    height: Dimensions.get('window').width * 3 / 4,
+    backgroundColor: '#EEE',
+    marginTop: 10,
+  },
+  image: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').width * 3 / 4,
+    resizeMode: 'center'
+  },
+  buttom: {
+    marginTop: 30,
+    padding: 10,
+    backgroundColor: '#4286f4'
+  },
+  buttomText: {
+    fontSize: 20,
+    color: '#FFF'
+  },
+  input: {
+    marginTop: 20,
+    width: '90%'
+  }
+})
+
+export default AddPhoto
