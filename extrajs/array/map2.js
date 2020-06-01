663291bb69910dced48ced728ac1f004afd36326
George Salu <george.salu10@gmail.com>
2020-06-01 17:02:03 -0300

    map #02 - desafio

---
+++ extrajs/array/map2.js
@@ -0,0 +1,8 @@
+const carrinho = [
+  '{ "nome": "Borracha", "preco": 3.45 }',
+  '{ "nome": "Caderno", "preco": 1.45 }',
+  '{ "nome": "Kit de Lapis", "preco": 23.45 }',
+  '{ "nome": "Caneta", "preco": 7.5 }'
+]
+
+// retorna um array apenas com os precos
