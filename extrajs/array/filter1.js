fa5f37ae5693d9e9aa35952b0ee2199b21fe3ee6
George Salu <george.salu10@gmail.com>
2020-06-01 17:45:48 -0300

    filter #01

---
+++ extrajs/array/filter1.js
@@ -0,0 +1,6 @@
+const produtos = [
+  { nome: 'Notebook', preco: 22331, fragil: true },
+  { nome: 'Ipad Pro', preco: 4331, fragil: true },
+  { nome: 'Copo de vidro', preco: 1331, fragil: true },
+  { nome: 'Copo de plastico', preco: 18.331, fragil: false }
+]
