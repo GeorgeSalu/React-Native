16497817b3dbcd4054f288bab989bcf3dab58750
George Salu <george.salu10@gmail.com>
2020-06-02 10:50:00 -0300

    filter #01

--- extrajs/array/filter1.js
+++ extrajs/array/filter1.js
@@ -4,3 +4,12 @@ const produtos = [
   { nome: 'Copo de vidro', preco: 1331, fragil: true },
   { nome: 'Copo de plastico', preco: 18.331, fragil: false }
 ]
+
+console.log(produtos.filter(function(p) {
+  return false
+}))
+
+const caro = produto => produto.preco >= 500
+const fragil = produto => produto.fragil
+
+console.log(produtos.filter(caro).filter(fragil))
