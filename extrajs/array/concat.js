947d4e92d2fe0af1c76859cbf174036f35903326
George Salu <george.salu10@gmail.com>
2020-06-03 14:11:49 -0300

    concat

--- extrajs/array/concat.js
+++ extrajs/array/concat.js
@@ -1,4 +1,4 @@
 const filhas = ['Valeskah', 'Cibalena']
 const filhos = ['Uoxiton', 'Vesclei']
-const total = filhas.concat(filhos)
-console.log(total)
+const todos = filhas.concat(filhos)
+console.log(todos)
