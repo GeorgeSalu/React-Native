6c5cfb7224b59015e7348b692aed43f728e696ee
George Salu <george.salu10@gmail.com>
2020-06-07 17:22:58 -0300

    removendo imports não utilizados

--- lambe/index.js
+++ lambe/index.js
@@ -1,14 +1,10 @@
-/** @format */
-
 import React from 'react'
 import { Provider } from 'react-redux'
 import {AppRegistry} from 'react-native';
-import Feed from './src/screens/Feed'
 import Navigator from './src/Navigator'
 import {name as appName} from './app.json';
 
 import storeConfig from './src/store/storeConfig'
-import Profile from './src/screens/Profile';
 
 const store = storeConfig()
 const Redux = () => (
