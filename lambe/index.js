400582fc7ef74e57d1d38fee011ca368769b23db
George Salu <george.salu10@gmail.com>
2020-06-07 17:41:40 -0300

    fix lint

--- lambe/index.js
+++ lambe/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import Navigator from './src/Navigator'
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 
 import storeConfig from './src/store/storeConfig'
 
