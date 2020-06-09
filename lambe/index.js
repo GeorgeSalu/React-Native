4233a89d0b11895b43c544ec9fd9b70ae65a46f6
George Salu <george.salu10@gmail.com>
2020-06-09 17:13:53 -0300

    config axios

--- lambe/index.js
+++ lambe/index.js
@@ -6,6 +6,10 @@ import { name as appName } from './app.json';
 
 import storeConfig from './src/store/storeConfig'
 
+import axios from 'axios'
+axios.defaults.baseURL = 'https://lambe-bedee.firebaseio.com/'
+
+
 const store = storeConfig()
 const Redux = () => (
   <Provider store={store}>
