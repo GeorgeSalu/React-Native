d6a2758b530bb3237041466408be5d9bcfa1cf0c
George Salu <george.salu10@gmail.com>
2020-06-09 11:14:41 -0300

    adicionando validações

--- lambe/src/components/Post.js
+++ lambe/src/components/Post.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import {
   StyleSheet,
   View,
@@ -11,12 +12,14 @@ import AddComments from './AddComment'
 
 class Post extends Component {
   render() {
+    const addComment = this.props.name ?
+      <AddComments postId={this.props.id} /> : null
     return (
       <View style={styles.container}>
         <Image source={this.props.image} style={styles.image} />
         <Author email={this.props.email} nickname={this.props.nickname} />
         <Comments comments={this.props.comments} />
-        <AddComments postId={this.props.id} />
+        {addComment}
       </View>
     )
   }
@@ -32,5 +35,14 @@ const styles = StyleSheet.create({
     resizeMode: 'contain'
   }
 })
+
+
+const mapStateToProps = ({ user }) => {
+  return {
+    name: user.name
+  }
+}
+
+export default connect(mapStateToProps)(Post)
 
 export default Post
