112df1564f6ffcd6a2fa04383f25bd24b8adc008
George Salu <george.salu10@gmail.com>
2020-06-09 22:10:02 -0300

    correção

--- lambe/index.js
+++ lambe/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { AppRegistry } from 'react-native';
 import Navigator from './src/Navigator'
-import { name as appName } from './app.json';s
+import { name as appName } from './app.json';
 import storeConfig from './src/store/storeConfig'
 import axios from 'axios'
 axios.defaults.baseURL = 'https://lambe-bedee.firebaseio.com/'
