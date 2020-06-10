43a1dccc1f1e7e5dbb1e2a00423947fc0a1a4ef3
George Salu <george.salu10@gmail.com>
2020-06-09 22:58:49 -0300

    usando o firebase Storage #02

--- lambe/functions/index.js
+++ lambe/functions/index.js
@@ -1,8 +1,20 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true })
+const fs = require('fs')
+const uuid = require('uuid')
+const { Storage } = require('@google-cloud/storage')
+const storage = new Storage({
+  projectId: 'lambe-bedee',
+  keyFilename: 'lambe-bedee.json'
+})
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+
+  cors(request, response, () => {
+    
+  })
+
+});
