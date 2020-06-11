ca5734376b4e7f71704619d44027bb30617fbe9f
George Salu <george.salu10@gmail.com>
2020-06-11 16:01:41 -0300

    removendo linha comentario

--- lambe/functions/index.js
+++ lambe/functions/index.js
@@ -10,7 +10,6 @@ const storage = new Storage({
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
 exports.helloWorld = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
     try {
