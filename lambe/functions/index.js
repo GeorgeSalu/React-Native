50eefeac6033fc5c3e34b49373504d65cda3f660
George Salu <george.salu10@gmail.com>
2020-06-11 15:21:54 -0300

    usando firebase Storage #03

--- lambe/functions/index.js
+++ lambe/functions/index.js
@@ -13,6 +13,34 @@ const storage = new Storage({
 //
 exports.helloWorld = functions.https.onRequest((request, response) => {
   cors(request, response, () => {
+    try {
+      fs.writeFileSync('/tmp/imageToSave.jpg', request.body.image, 'base64')
 
+      const bucket = storage.bucket('lambe-bedee.appspot.com')
+      const id = uuid()
+      bucket.upload('/tmp/imageToSave.jpg', {
+        uploadType: 'media',
+        destination: `/posts/${id}.jpg`,
+        metadata: {
+          metadata: {
+            contentType: 'image/jpg',
+            firebaseStorageDownloadTokens: id
+          }
+        }
+      }, (err, file) => {
+        if(err) {
+          console.log(err)
+          return response.status(500).json({ error: err })
+        } else {
+          const filename = encodeURIComponent(file.name)
+          const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/'
+            + bucket.name + '/o/' + filename + '?' + '?alt=media&token=' + id
+          return response.status(201).json({ imageUrl: imageUrl })
+        }
+      })
+    } catch (error) {
+      console.log(error)
+      return response.status(500).json({ error: error })
+    }
   })
 });
