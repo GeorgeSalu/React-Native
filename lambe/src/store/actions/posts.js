1ee43f6f4ae35a2c443e5c571bdbbd382caa5543
George Salu <george.salu10@gmail.com>
2020-06-11 22:35:54 -0300

    usando firebase Storage #4

--- lambe/src/store/actions/posts.js
+++ lambe/src/store/actions/posts.js
@@ -3,9 +3,21 @@ import axios from 'axios'
 
 export const addPost = post => {
   return dispatch => {
-    axios.post('/posts.json', { ...post })
+    axios({
+      url: 'uploadImage',
+      baseURL: 'https://us-central1-lambe-bedee.cloudfunctions.net',
+      method: 'post',
+      data: {
+        image: post.image.base64
+      }
+    })
       .catch(err => console.log(err))
-      .then(res => console.log(res.data))
+      .then(res => {
+        post.image = res.data.imageUrl
+        axios.post('/posts.json', { ...post })
+          .catch(err => console.log(err))
+          .then(res => console.log(res.data))
+      })
   }
   
   /*return {
